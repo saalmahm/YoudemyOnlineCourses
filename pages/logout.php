d6ca0a07f33ab5ab16037efb88279a994c0a4dc6
salma hamdi <saalmahm51@gmail.com>
2025-01-21 23:06:58 +0100

    Des petits amélioration

--- pages/logout.php
+++ pages/logout.php
@@ -1,12 +1,10 @@
 <?php
 require_once '../classes/User.php';
 
-// Démarrer la session et se déconnecter
 session_start();
 session_unset();
 session_destroy();
 
-// Redirection vers la page de connexion
 header("Location: /login.php");
 exit();
 ?>
--- pages/signup.php
+++ pages/signup.php
@@ -41,43 +41,43 @@
 </header>
 
 <section class="bg-green-100 h-screen mt-8">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-full">
-      <div class="w-full max-w-md bg-white rounded-lg shadow-md">
-          <div class="p-6 space-y-6">
-              <h1 class="text-2xl font-bold text-center text-green-700">
-                  Create an Account
-              </h1>
-              <form class="space-y-6" action="./signupform.php"  method="post">
-    <div>
-        <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
-        <input type="text" name="username" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" placeholder="Enter your name" required>
-    </div>
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-full">
+            <div class="w-full max-w-md bg-white rounded-lg shadow-md">
+                <div class="p-6 space-y-6">
+                    <h1 class="text-2xl font-bold text-center text-green-700">
+                        Create an Account
+                    </h1>
+<form class="space-y-6" action="./signupform.php" method="post">
+        <div>
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
+            <input type="text" name="username" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" placeholder="Enter your name" required>
+        </div>
 
-    <div>
-        <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
-        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" placeholder="Enter your email" required>
-    </div>
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
+            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" placeholder="Enter your email" required>
+        </div>
 
-    <div>
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-        <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" placeholder="Enter your password" required>
-    </div>
+        <div>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" placeholder="Enter your password" required>
+        </div>
 
-    <div>
-        <label for="role" class="block mb-2 text-sm font-medium text-gray-900">Select Role</label>
-        <select name="role" id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" required>
-            <option value="" disabled selected>Select your role</option>
-            <option value="étudiant">Student</option>
-            <option value="enseignant">Teacher</option>
-        </select>
-    </div>
+        <div>
+            <label for="role" class="block mb-2 text-sm font-medium text-gray-900">Select Role</label>
+            <select name="role" id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2.5" required>
+                <option value="" disabled selected>Select your role</option>
+                <option value="étudiant">Student</option>
+                <option value="enseignant">Teacher</option>
+            </select>
+        </div>
 
-<button type="submit" class="w-full bg-green-600 hover:bg-green-700 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an Account</button>
+        <button type="submit" class="w-full bg-green-600 hover:bg-green-700 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an Account</button>
 
-    <p class="text-sm font-light text-gray-500 text-center">
-        Already have an account? 
-        <a href="login.php" class="font-medium text-green-700 hover:underline">Login here</a>
-    </p>
+        <p class="text-sm font-light text-gray-500 text-center">
+            Already have an account? 
+            <a href="login.php" class="font-medium text-green-700 hover:underline">Login here</a>
+        </p>
 </form>
 
           </div>
